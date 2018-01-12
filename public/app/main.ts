7563649047b45d43c74ad492750d1bb10996aeeb
Daniel Rodas <daniel.rodas1@hotmail.com>
2018-01-12 10:17:53 +0000

    Seperating API from application in preperation for AngularJS implementation

--- public/app/main.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Rodas on 7/4/2016.
- */
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app.module';
-
-const platform = platformBrowserDynamic();
-platform.bootstrapModule(AppModule);
